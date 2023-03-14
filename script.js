2f0cce00f9a5b875f48c21f77818696e473362a1
Mohanekm2001 <124660796+Mohanekm2001@users.noreply.github.com>
2023-03-14 10:50:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-let yourArray = ['apple', 25, true, 'banana', false];
-
-// Add each element of yourArray to the unordered list in the HTML
-for (let i = 0; i < yourArray.length; i++) {
-  let listItem = document.createElement('li');
-  listItem.textContent = yourArray[i];
-  document.getElementById('list').appendChild(listItem);
-}
+let yourArray = [1, 'two', true, null, false];
+console.log(yourArray.length); // logs 5
