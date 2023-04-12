f109192dd3b5ca0f7822933fb616bd96a78c6982
Mohanekm2001 <124660796+Mohanekm2001@users.noreply.github.com>
2023-04-12 11:46:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-let yourArray = [1, 'two', true, null, false];
-console.log(yourArray.length); // logs 5
+// Define an array with at least 5 elements containing a string, a number, and a boolean
+let yourArray = ['apple', 123, true, 'orange', false];
+
+// Accessing elements in the array
+console.log(yourArray[0]); // logs 'apple'
+console.log(yourArray[1]); // logs 123
+console.log(yourArray[2]); // logs true
+
+// Modifying elements in the array
+yourArray[3] = 'banana';
+console.log(yourArray); // logs ['apple', 123, true, 'banana', false]
+
+// Adding elements to the array
+yourArray.push(456);
+console.log(yourArray); // logs ['apple', 123, true, 'banana', false, 456]
+
+// Removing elements from the array
+yourArray.pop();
+console.log(yourArray); // logs ['apple', 123, true, 'banana', false]
